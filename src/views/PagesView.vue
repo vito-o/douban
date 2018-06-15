7e8d84932ee70c687c32bee5adc7902c2b1fc778
vito-o <taiwangwan@163.com>
2018-06-15 17:29:09 +0800

    start douban project

---
+++ src/views/PagesView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="pages-view">
+    <HeaderBar @showTalion="open"></HeaderBar>
+    <router-view></router-view>
+    <talion-view v-show="talion" @showTalion="close"></talion-view>
+  </div>
+</template>
+
+<script>
+import HeaderBar from '@/components/HeaderBar';
+import TalionView from '@/views/TalionView';
+
+export default {
+  components: {
+    HeaderBar,
+    TalionView,
+  },
+  data() {
+    return {
+      talion: '',
+    };
+  },
+  methods: {
+    open() {
+      this.talion = 'open';
+    },
+    close() {
+      this.talion = '';
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
